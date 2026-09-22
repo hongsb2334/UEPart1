9b5d370eeccb6d25b54178f17c32a4af11ed4711
hongsb2334 <hongsb2334@gmail.com>
2026-09-22 12:03:48 +0900

    c++ 프로젝트 생성

---
+++ Source/UEPart1/MyGameInstance.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+
+    //언리얼은 utf16->wchar만 사용
+    // 
+    // 각종 타입으로 변경할 수 있는 헬퍼 함수 제공
+    //UE_LOG(LogTemp, Warning, TEXT("hello unreal"));
+
+    //TCHAR | FSTRING
+    TCHAR LogCharArray[] = TEXT("Hello Unreal");
+    UE_LOG(LogTemp, Warning, TEXT("%s"), LogCharArray);
+
+    FString LogCharString = LogCharArray;
+    UE_LOG(LogTemp, Warning, TEXT("%s"), *LogCharString);
+}
